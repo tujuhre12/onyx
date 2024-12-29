568bc16536754162cd1a50cfb9be5904936e8a49
joachim-danswer <joachim@danswer.ai>
2024-12-29 10:51:31 -0800

    all 3 options

--- backend/onyx/agent_search/main/graph_builder.py
+++ backend/onyx/agent_search/main/graph_builder.py
@@ -3,14 +3,21 @@ from langgraph.graph import START
 from langgraph.graph import StateGraph
 
 from onyx.agent_search.answer_question.graph_builder import answer_query_graph_builder
+from onyx.agent_search.base_raw_search.graph_builder import (
+    base_raw_search_graph_builder,
+)
 from onyx.agent_search.main.edges import parallelize_decompozed_answer_queries
 from onyx.agent_search.main.nodes.base_decomp import main_decomp_base
 from onyx.agent_search.main.nodes.generate_initial_answer import (
     generate_initial_answer,
 )
 from onyx.agent_search.main.nodes.ingest_answers import ingest_answers
+from onyx.agent_search.main.nodes.ingest_initial_retrieval import (
+    ingest_initial_retrieval,
+)
 from onyx.agent_search.main.states import MainInput
 from onyx.agent_search.main.states import MainState
+
 
 test_mode = False
 
@@ -21,136 +28,402 @@ def main_graph_builder(test_mode: bool = False) -> StateGraph:
         input=MainInput,
     )
 
-    ### Add nodes ###
+    graph_component = "both"
+    # graph_component = "right"
+    # graph_component = "left"
 
-    graph.add_node(
-        node="base_decomp",
-        action=main_decomp_base,
-    )
-    answer_query_subgraph = answer_query_graph_builder().compile()
-    graph.add_node(
-        node="answer_query",
-        action=answer_query_subgraph,
-    )
+    if graph_component == "left":
+        ### Add nodes ###
 
-    # graph.add_node(
-    #     node="prep_for_initial_retrieval",
-    #     action=prep_for_initial_retrieval,
-    # )
+        graph.add_node(
+            node="base_decomp",
+            action=main_decomp_base,
+        )
+        answer_query_subgraph = answer_query_graph_builder().compile()
+        graph.add_node(
+            node="answer_query",
+            action=answer_query_subgraph,
+        )
 
-    # expanded_retrieval_subgraph = expanded_retrieval_graph_builder().compile()
-    # graph.add_node(
-    #     node="initial_retrieval",
-    #     action=expanded_retrieval_subgraph,
-    # )
+        # graph.add_node(
+        #     node="prep_for_initial_retrieval",
+        #     action=prep_for_initial_retrieval,
+        # )
 
-    # base_raw_search_subgraph = base_raw_search_graph_builder().compile()
-    # graph.add_node(
-    #     node="base_raw_search_data",
-    #     action=base_raw_search_subgraph,
-    # )
-    # graph.add_node(
-    #     node="ingest_initial_retrieval",
-    #     action=ingest_initial_retrieval,
-    # )
-    graph.add_node(
-        node="ingest_answers",
-        action=ingest_answers,
-    )
-    graph.add_node(
-        node="generate_initial_answer",
-        action=generate_initial_answer,
-    )
-    # if test_mode:
-    #     graph.add_node(
-    #         node="generate_initial_base_answer",
-    #         action=generate_initial_base_answer,
-    #     )
+        # expanded_retrieval_subgraph = expanded_retrieval_graph_builder().compile()
+        # graph.add_node(
+        #     node="initial_retrieval",
+        #     action=expanded_retrieval_subgraph,
+        # )
 
-    ### Add edges ###
+        # base_raw_search_subgraph = base_raw_search_graph_builder().compile()
+        # graph.add_node(
+        #     node="base_raw_search_data",
+        #     action=base_raw_search_subgraph,
+        # )
+        # graph.add_node(
+        #     node="ingest_initial_retrieval",
+        #     action=ingest_initial_retrieval,
+        # )
+        graph.add_node(
+            node="ingest_answers",
+            action=ingest_answers,
+        )
+        graph.add_node(
+            node="generate_initial_answer",
+            action=generate_initial_answer,
+        )
+        # if test_mode:
+        #     graph.add_node(
+        #         node="generate_initial_base_answer",
+        #         action=generate_initial_base_answer,
+        #     )
 
-    # graph.add_conditional_edges(
-    #     source=START,
-    #     path=send_to_initial_retrieval,
-    #     path_map=["initial_retrieval"],
-    # )
+        ### Add edges ###
 
-    # graph.add_edge(
-    #     start_key=START,
-    #     end_key="prep_for_initial_retrieval",
-    # )
-    # graph.add_edge(
-    #     start_key="prep_for_initial_retrieval",
-    #     end_key="initial_retrieval",
-    # )
-    # graph.add_edge(
-    #     start_key="initial_retrieval",
-    #     end_key="ingest_initial_retrieval",
-    # )
+        # graph.add_conditional_edges(
+        #     source=START,
+        #     path=send_to_initial_retrieval,
+        #     path_map=["initial_retrieval"],
+        # )
 
-    # graph.add_edge(
-    #     start_key=START,
-    #     end_key="base_raw_search_data"
-    # )
+        # graph.add_edge(
+        #     start_key=START,
+        #     end_key="prep_for_initial_retrieval",
+        # )
+        # graph.add_edge(
+        #     start_key="prep_for_initial_retrieval",
+        #     end_key="initial_retrieval",
+        # )
+        # graph.add_edge(
+        #     start_key="initial_retrieval",
+        #     end_key="ingest_initial_retrieval",
+        # )
 
-    # # graph.add_edge(
-    # #     start_key="base_raw_search_data",
-    # #     end_key=END
-    # # )
-    # graph.add_edge(
-    #     start_key="base_raw_search_data",
-    #     end_key="ingest_initial_retrieval",
-    # )
-    # graph.add_edge(
-    #     start_key="ingest_initial_retrieval",
-    #     end_key=END
-    # )
-    graph.add_edge(
-        start_key=START,
-        end_key="base_decomp",
-    )
-    graph.add_conditional_edges(
-        source="base_decomp",
-        path=parallelize_decompozed_answer_queries,
-        path_map=["answer_query"],
-    )
-    graph.add_edge(
-        start_key="answer_query",
-        end_key="ingest_answers",
-    )
+        # graph.add_edge(
+        #     start_key=START,
+        #     end_key="base_raw_search_data"
+        # )
 
-    graph.add_edge(
-        start_key="ingest_answers",
-        end_key="generate_initial_answer",
-    )
+        # # graph.add_edge(
+        # #     start_key="base_raw_search_data",
+        # #     end_key=END
+        # # )
+        # graph.add_edge(
+        #     start_key="base_raw_search_data",
+        #     end_key="ingest_initial_retrieval",
+        # )
+        # graph.add_edge(
+        #     start_key="ingest_initial_retrieval",
+        #     end_key=END
+        # )
+        graph.add_edge(
+            start_key=START,
+            end_key="base_decomp",
+        )
+        graph.add_conditional_edges(
+            source="base_decomp",
+            path=parallelize_decompozed_answer_queries,
+            path_map=["answer_query"],
+        )
+        graph.add_edge(
+            start_key="answer_query",
+            end_key="ingest_answers",
+        )
 
-    # graph.add_edge(
-    #     start_key=["ingest_answers", "ingest_initial_retrieval"],
-    #     end_key="generate_initial_answer",
-    # )
+        graph.add_edge(
+            start_key="ingest_answers",
+            end_key="generate_initial_answer",
+        )
 
-    graph.add_edge(
-        start_key="generate_initial_answer",
-        end_key=END,
-    )
-    # graph.add_edge(
-    #     start_key="ingest_answers",
-    #     end_key="generate_initial_answer",
-    # )
-    # if test_mode:
-    #     graph.add_edge(
-    #         start_key=["ingest_answers", "ingest_initial_retrieval"],
-    #         end_key="generate_initial_base_answer",
-    #     )
-    #     graph.add_edge(
-    #         start_key=["generate_initial_answer", "generate_initial_base_answer"],
-    #         end_key=END,
-    #     )
-    # else:
-    #     graph.add_edge(
-    #         start_key="generate_initial_answer",
-    #         end_key=END,
-    #     )
+        # graph.add_edge(
+        #     start_key=["ingest_answers", "ingest_initial_retrieval"],
+        #     end_key="generate_initial_answer",
+        # )
+
+        graph.add_edge(
+            start_key="generate_initial_answer",
+            end_key=END,
+        )
+        # graph.add_edge(
+        #     start_key="ingest_answers",
+        #     end_key="generate_initial_answer",
+        # )
+        # if test_mode:
+        #     graph.add_edge(
+        #         start_key=["ingest_answers", "ingest_initial_retrieval"],
+        #         end_key="generate_initial_base_answer",
+        #     )
+        #     graph.add_edge(
+        #         start_key=["generate_initial_answer", "generate_initial_base_answer"],
+        #         end_key=END,
+        #     )
+        # else:
+        #     graph.add_edge(
+        #         start_key="generate_initial_answer",
+        #         end_key=END,
+        #     )
+
+    elif graph_component == "right":
+        ### Add nodes ###
+
+        # graph.add_node(
+        #     node="base_decomp",
+        #     action=main_decomp_base,
+        # )
+        # answer_query_subgraph = answer_query_graph_builder().compile()
+        # graph.add_node(
+        #     node="answer_query",
+        #     action=answer_query_subgraph,
+        # )
+
+        # graph.add_node(
+        #     node="prep_for_initial_retrieval",
+        #     action=prep_for_initial_retrieval,
+        # )
+
+        # expanded_retrieval_subgraph = expanded_retrieval_graph_builder().compile()
+        # graph.add_node(
+        #     node="initial_retrieval",
+        #     action=expanded_retrieval_subgraph,
+        # )
+
+        base_raw_search_subgraph = base_raw_search_graph_builder().compile()
+        graph.add_node(
+            node="base_raw_search_data",
+            action=base_raw_search_subgraph,
+        )
+        graph.add_node(
+            node="ingest_initial_retrieval",
+            action=ingest_initial_retrieval,
+        )
+        # graph.add_node(
+        #     node="ingest_answers",
+        #     action=ingest_answers,
+        # )
+        graph.add_node(
+            node="generate_initial_answer",
+            action=generate_initial_answer,
+        )
+        # if test_mode:
+        #     graph.add_node(
+        #         node="generate_initial_base_answer",
+        #         action=generate_initial_base_answer,
+        #     )
+
+        ### Add edges ###
+
+        # graph.add_conditional_edges(
+        #     source=START,
+        #     path=send_to_initial_retrieval,
+        #     path_map=["initial_retrieval"],
+        # )
+
+        # graph.add_edge(
+        #     start_key=START,
+        #     end_key="prep_for_initial_retrieval",
+        # )
+        # graph.add_edge(
+        #     start_key="prep_for_initial_retrieval",
+        #     end_key="initial_retrieval",
+        # )
+        # graph.add_edge(
+        #     start_key="initial_retrieval",
+        #     end_key="ingest_initial_retrieval",
+        # )
+
+        graph.add_edge(start_key=START, end_key="base_raw_search_data")
+
+        # # graph.add_edge(
+        # #     start_key="base_raw_search_data",
+        # #     end_key=END
+        # # )
+        graph.add_edge(
+            start_key="base_raw_search_data",
+            end_key="ingest_initial_retrieval",
+        )
+        # graph.add_edge(
+        #     start_key="ingest_initial_retrieval",
+        #     end_key=END
+        # )
+        # graph.add_edge(
+        #     start_key=START,
+        #     end_key="base_decomp",
+        # )
+        # graph.add_conditional_edges(
+        #     source="base_decomp",
+        #     path=parallelize_decompozed_answer_queries,
+        #     path_map=["answer_query"],
+        # )
+        # graph.add_edge(
+        #     start_key="answer_query",
+        #     end_key="ingest_answers",
+        # )
+
+        # graph.add_edge(
+        #     start_key="ingest_answers",
+        #     end_key="generate_initial_answer",
+        # )
+
+        graph.add_edge(
+            start_key="ingest_initial_retrieval",
+            end_key="generate_initial_answer",
+        )
+
+        # graph.add_edge(
+        #     start_key=["ingest_answers", "ingest_initial_retrieval"],
+        #     end_key="generate_initial_answer",
+        # )
+
+        graph.add_edge(
+            start_key="generate_initial_answer",
+            end_key=END,
+        )
+        # graph.add_edge(
+        #     start_key="ingest_answers",
+        #     end_key="generate_initial_answer",
+        # )
+        # if test_mode:
+        #     graph.add_edge(
+        #         start_key=["ingest_answers", "ingest_initial_retrieval"],
+        #         end_key="generate_initial_base_answer",
+        #     )
+        #     graph.add_edge(
+        #         start_key=["generate_initial_answer", "generate_initial_base_answer"],
+        #         end_key=END,
+        #     )
+        # else:
+        #     graph.add_edge(
+        #         start_key="generate_initial_answer",
+        #         end_key=END,
+        #     )
+
+    else:
+        graph.add_node(
+            node="base_decomp",
+            action=main_decomp_base,
+        )
+        answer_query_subgraph = answer_query_graph_builder().compile()
+        graph.add_node(
+            node="answer_query",
+            action=answer_query_subgraph,
+        )
+
+        # graph.add_node(
+        #     node="prep_for_initial_retrieval",
+        #     action=prep_for_initial_retrieval,
+        # )
+
+        # expanded_retrieval_subgraph = expanded_retrieval_graph_builder().compile()
+        # graph.add_node(
+        #     node="initial_retrieval",
+        #     action=expanded_retrieval_subgraph,
+        # )
+
+        base_raw_search_subgraph = base_raw_search_graph_builder().compile()
+        graph.add_node(
+            node="base_raw_search_data",
+            action=base_raw_search_subgraph,
+        )
+        graph.add_node(
+            node="ingest_initial_retrieval",
+            action=ingest_initial_retrieval,
+        )
+        graph.add_node(
+            node="ingest_answers",
+            action=ingest_answers,
+        )
+        graph.add_node(
+            node="generate_initial_answer",
+            action=generate_initial_answer,
+        )
+        # if test_mode:
+        #     graph.add_node(
+        #         node="generate_initial_base_answer",
+        #         action=generate_initial_base_answer,
+        #     )
+
+        ### Add edges ###
+
+        # graph.add_conditional_edges(
+        #     source=START,
+        #     path=send_to_initial_retrieval,
+        #     path_map=["initial_retrieval"],
+        # )
+
+        # graph.add_edge(
+        #     start_key=START,
+        #     end_key="prep_for_initial_retrieval",
+        # )
+        # graph.add_edge(
+        #     start_key="prep_for_initial_retrieval",
+        #     end_key="initial_retrieval",
+        # )
+        # graph.add_edge(
+        #     start_key="initial_retrieval",
+        #     end_key="ingest_initial_retrieval",
+        # )
+
+        graph.add_edge(start_key=START, end_key="base_raw_search_data")
+
+        # # graph.add_edge(
+        # #     start_key="base_raw_search_data",
+        # #     end_key=END
+        # # )
+        graph.add_edge(
+            start_key="base_raw_search_data",
+            end_key="ingest_initial_retrieval",
+        )
+        # graph.add_edge(
+        #     start_key="ingest_initial_retrieval",
+        #     end_key=END
+        # )
+        graph.add_edge(
+            start_key=START,
+            end_key="base_decomp",
+        )
+        graph.add_conditional_edges(
+            source="base_decomp",
+            path=parallelize_decompozed_answer_queries,
+            path_map=["answer_query"],
+        )
+        graph.add_edge(
+            start_key="answer_query",
+            end_key="ingest_answers",
+        )
+
+        # graph.add_edge(
+        #     start_key="ingest_answers",
+        #     end_key="generate_initial_answer",
+        # )
+
+        graph.add_edge(
+            start_key=["ingest_answers", "ingest_initial_retrieval"],
+            end_key="generate_initial_answer",
+        )
+
+        graph.add_edge(
+            start_key="generate_initial_answer",
+            end_key=END,
+        )
+        # graph.add_edge(
+        #     start_key="ingest_answers",
+        #     end_key="generate_initial_answer",
+        # )
+        # if test_mode:
+        #     graph.add_edge(
+        #         start_key=["ingest_answers", "ingest_initial_retrieval"],
+        #         end_key="generate_initial_base_answer",
+        #     )
+        #     graph.add_edge(
+        #         start_key=["generate_initial_answer", "generate_initial_base_answer"],
+        #         end_key=END,
+        #     )
+        # else:
+        #     graph.add_edge(
+        #         start_key="generate_initial_answer",
+        #         end_key=END,
+        #     )
 
     return graph
 
