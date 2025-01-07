2d948812a5965d58d91b1e08651c459b6931ea9e
pablodanswer <pablo@danswer.ai>
2025-01-06 16:16:08 -0800

    nit

--- web/next.config.js
+++ web/next.config.js
@@ -32,26 +32,26 @@ const nextConfig = {
       {
         source: "/(.*)",
         headers: [
-          // {
-          //   key: "Content-Security-Policy",
-          //   value: cspHeader.replace(/\n/g, ""),
-          // },
-          // {
-          //   key: "Strict-Transport-Security",
-          //   value: "max-age=63072000; includeSubDomains; preload",
-          // },
-          // {
-          //   key: "Referrer-Policy",
-          //   value: "strict-origin-when-cross-origin",
-          // },
-          // {
-          //   key: "X-Frame-Options",
-          //   value: "DENY",
-          // },
-          // {
-          //   key: "X-Content-Type-Options",
-          //   value: "nosniff",
-          // },
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
           {
             key: "Permissions-Policy",
             // Deny all permissions by default
