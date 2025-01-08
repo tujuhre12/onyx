b850d0b23c54bd1c60da54091c0c15c1ce712c7f
pablodanswer <pablo@danswer.ai>
2025-01-07 16:22:50 -0800

    validate

--- web/src/lib/extension/constants.ts
+++ web/src/lib/extension/constants.ts
@@ -9,19 +9,20 @@ export const lightExtensionImages = [
   "https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ];
 
-// Local storage keys
-export const SHORTCUTS_KEY = "shortCuts";
-export const NEW_TAB_PAGE_VIEW_KEY = "newTabPageView";
-export const USE_ONYX_AS_NEW_TAB_KEY = "useOnyxAsNewTab";
-
-// Default values
-export const DEFAULT_LIGHT_BACKGROUND_IMAGE = "onyxBackgroundLight";
-export const DEFAULT_DARK_BACKGROUND_IMAGE = "onyxBackgroundDark";
-export const DEFAULT_NEW_TAB_PAGE_VIEW = "chat";
-
 // Chrome message types
 export const CHROME_MESSAGE = {
   PREFERENCES_UPDATED: "PREFERENCES_UPDATED",
   ONYX_APP_LOADED: "ONYX_APP_LOADED",
   SET_DEFAULT_NEW_TAB: "SET_DEFAULT_NEW_TAB",
+  LOAD_NEW_CHAT_PAGE: "LOAD_NEW_CHAT_PAGE",
+  LOAD_NEW_PAGE: "LOAD_NEW_PAGE",
+};
+
+export const LocalStorageKeys = {
+  THEME: "onyxTheme",
+  LIGHT_BG_URL: "lightBgUrl",
+  DARK_BG_URL: "darkBgUrl",
+  SHORTCUTS: "shortCuts",
+  SHOW_SHORTCUTS: "showShortcuts",
+  USE_ONYX_AS_NEW_TAB: "useOnyxAsDefaultNewTab",
 };
