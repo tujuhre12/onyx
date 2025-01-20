c0b8098c223e7409f292bc1a945b727e5d930f64
pablodanswer <pablo@danswer.ai>
2025-01-20 13:07:08 -0800

    k

--- web/sentry.client.config.ts
+++ web/sentry.client.config.ts
@@ -1,6 +1,5 @@
 import * as Sentry from "@sentry/nextjs";
 
-// Only do this if you actually have a DSN set
 if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
