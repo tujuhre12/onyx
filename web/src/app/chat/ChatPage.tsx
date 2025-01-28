16300b42a1cb11a70385e8f44a46e1b451e9ca5f
Evan Lohn <evan@danswer.ai>
2025-01-28 14:43:15 -0800

    remove debug

--- web/src/app/chat/ChatPage.tsx
+++ web/src/app/chat/ChatPage.tsx
@@ -2538,13 +2538,7 @@ export function ChatPage({
                                   >
                                     <HumanMessage
                                       stopGenerating={stopGenerating}
-                                      content={
-                                        message.message +
-                                        `final message index is ${
-                                          currentRegenerationState()
-                                            ?.finalMessageIndex
-                                        }`
-                                      }
+                                      content={message.message}
                                       files={message.files}
                                       messageId={message.messageId}
                                       onEdit={(editedContent) => {
