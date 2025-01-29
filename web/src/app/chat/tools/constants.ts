b5ea88c6bafec0f8ad569b38cfa479a23a046acc
pablodanswer <pablo@danswer.ai>
2025-01-28 21:53:26 -0800

    tool id constants

--- web/src/app/chat/tools/constants.ts
+++ web/src/app/chat/tools/constants.ts
@@ -1,3 +1,7 @@
 export const SEARCH_TOOL_NAME = "run_search";
 export const INTERNET_SEARCH_TOOL_NAME = "run_internet_search";
 export const IMAGE_GENERATION_TOOL_NAME = "run_image_generation";
+
+export const SEARCH_TOOL_ID = "SearchTool";
+export const IIMAGE_GENERATION_TOOL_ID = "ImageGenerationTool";
+export const INTERNET_SEARCH_TOOL_ID = "InternetSearchTool";
