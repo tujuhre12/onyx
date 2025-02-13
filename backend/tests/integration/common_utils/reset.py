702edce4db44b836f98c3a07c9daa0646348b1da
Weves <chrisweaver101@gmail.com>
2025-02-12 16:48:03 -0800

    test closing idle only

--- backend/tests/integration/common_utils/reset.py
+++ backend/tests/integration/common_utils/reset.py
@@ -93,6 +93,7 @@ def downgrade_postgres(
             SELECT pg_terminate_backend(pg_stat_activity.pid)
             FROM pg_stat_activity
             WHERE pg_stat_activity.datname = '{database}'
+            AND pg_stat_activity.state = 'idle in transaction'
             AND pid <> pg_backend_pid();
         """
         )
