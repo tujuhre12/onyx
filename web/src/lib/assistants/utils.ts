397af75661b766429f5356416435ef110e253926
pablonyx <pablo@danswer.ai>
2025-02-27 15:26:49 -0800

    minor assistant filtering fix

--- web/src/lib/assistants/utils.ts
+++ web/src/lib/assistants/utils.ts
@@ -129,7 +129,6 @@ export function filterAssistants(
     (assistant) => assistant.is_visible
   );
 
-  // console.log("hasAnyConnectors", hasAnyConnectors);
   if (!hasAnyConnectors) {
     filteredAssistants = filteredAssistants.filter(
       (assistant) =>
