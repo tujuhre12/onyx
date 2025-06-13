4812be01bb2665ba17c7bd366c86732c4c98094e
Richard Kuo (Onyx) <rkuo@onyx.app>
2025-06-13 13:12:43 -0700

    tweaks to openapi generation

--- backend/scripts/onyx_openapi_schema.py
+++ backend/scripts/onyx_openapi_schema.py
@@ -4,28 +4,30 @@
 
 import argparse
 import json
+import os
 
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 
 from onyx.main import app as app_fn
 
+# Force full enterprise edition schema to be generated
+os.environ["ENABLE_PAID_ENTERPRISE_EDITION_FEATURES"] = "True"
 
-def go(filename: str) -> None:
-    with open(filename, "w") as f:
-        app: FastAPI = app_fn()
-        json.dump(
-            get_openapi(
-                title=app.title,
-                version=app.version,
-                openapi_version=app.openapi_version,
-                description=app.description,
-                routes=app.routes,
-            ),
-            f,
-        )
 
-    print(f"Wrote OpenAPI schema to {filename}.")
+def go(filenames: list[str]) -> None:
+    app: FastAPI = app_fn()
+    openapi_schema = get_openapi(
+        title=app.title,
+        version=app.version,
+        openapi_version=app.openapi_version,
+        description=app.description,
+        routes=app.routes,
+    )
+    for filename in filenames:
+        with open(filename, "w") as f:
+            json.dump(openapi_schema, f, indent=2)
+        print(f"Wrote OpenAPI schema to {filename}.")
 
 
 def main() -> None:
@@ -33,11 +35,15 @@ def main() -> None:
         description="Export OpenAPI schema for Onyx API (does not require starting API server)"
     )
     parser.add_argument(
-        "--filename", "-f", help="Filename to write to", default="openapi.json"
+        "--filenames",
+        "-f",
+        help="Filenames to write to. Can specify multiple delimited by spaces.",
+        nargs="+",
+        default=["openapi.json"],
     )
 
     args = parser.parse_args()
-    go(args.filename)
+    go(args.filenames)
 
 
 if __name__ == "__main__":
