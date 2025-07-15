29b608b3be0bf96b15e4b9bd4b6ad0a82067c582
Evan Lohn <evan@danswer.ai>
2025-07-15 16:10:49 -0700

    cosmetic fixes

--- backend/onyx/background/celery/tasks/indexing/tasks.py
+++ backend/onyx/background/celery/tasks/indexing/tasks.py
@@ -426,6 +426,7 @@ def check_indexing_completion(
             )
 
         if attempt.status.is_successful():
+            cc_pair.last_successful_index_time = attempt.poll_range_end
             if cc_pair.status in [
                 ConnectorCredentialPairStatus.SCHEDULED,
                 ConnectorCredentialPairStatus.INITIAL_INDEXING,
@@ -437,6 +438,8 @@ def check_indexing_completion(
             if cc_pair.in_repeated_error_state:
                 cc_pair.in_repeated_error_state = False
                 db_session.commit()
+
+            # TODO: make it so we don't need this (might already be true)
             redis_connector = RedisConnector(
                 tenant_id, attempt.connector_credential_pair_id
             )
