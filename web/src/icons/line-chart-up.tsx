d5a222925a5a56d6ca103bfe6dc0aee87934fb7b
Raunak Bhagat <r@rabh.io>
2025-09-16 09:51:25 -0700

    Add icons (as raw TSX)

---
+++ web/src/icons/line-chart-up.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgLineChartUp = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M13 6.5L13 3M13 3H9.5M13 3L7.99999 8L6.49999 6.5L3 10M3 13H13"
+      strokeOpacity={0.8}
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+export default SvgLineChartUp;
