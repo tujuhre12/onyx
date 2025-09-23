05ecb0200f031ecf5ac1a49cd72015ba46ae39dd
edwin <edwin@onyx.app>
2025-09-22 19:11:20 -0700

    .

--- backend/onyx/context/search/utils.py
+++ backend/onyx/context/search/utils.py
@@ -2,8 +2,6 @@ import string
 from collections.abc import Sequence
 from typing import TypeVar
 
-from nltk.corpus import stopwords  # type:ignore
-from nltk.tokenize import word_tokenize  # type:ignore
 from sqlalchemy.orm import Session
 
 from onyx.chat.models import SectionRelevancePiece
@@ -153,6 +151,9 @@ def chunks_or_sections_to_search_docs(
 
 
 def remove_stop_words_and_punctuation(keywords: list[str]) -> list[str]:
+    from nltk.corpus import stopwords  # type: ignore[import-untyped]
+    from nltk.tokenize import word_tokenize  # type: ignore[import-untyped]
+
     try:
         # Re-tokenize using the NLTK tokenizer for better matching
         query = " ".join(keywords)
